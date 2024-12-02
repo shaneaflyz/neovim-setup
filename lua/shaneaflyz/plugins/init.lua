9ae7d8662197bd022001e2f01b8835d1175d5731
shaneaflyz <shaneaflyz@gmail.com>
2024-12-02 21:34:46 +0800

    refactor: to-do plugin placed in init.lua instead of separate file

--- lua/shaneaflyz/plugins/init.lua
+++ lua/shaneaflyz/plugins/init.lua
@@ -1,7 +1,7 @@
 -- for plugins that DO NOT require any/much configuration
--- import below in this init.lua
 
 return {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
   { 'Bilal2453/luvit-meta', lazy = true },
 }
